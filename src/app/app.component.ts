bed3f1e4c709c3d3ca5b98ef675484b441014c08
Eyerusalem Tesfaye <tesfayerusalem@gmail.com>
2023-11-13 12:37:11 +0300

    bootstrap added to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import { ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import * as bootstrap from 'bootstrap';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'portifilo';
+  @ViewChild('helloModal') helloEl?: ElementRef;
+  modal?: bootstrap.Modal;
+  ngAfterViewInit() {
+    this.modal = new bootstrap.Modal(this.helloEl?.nativeElement, {});
+  }
+
+  trigger() {
+    this.modal?.toggle();
+  }
 }
